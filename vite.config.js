4006641f3db073493b8df7fde5e135feefdbd4f0
himansh025 <himanshudeolia0825@gmail.com>
2024-11-03 10:52:26 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import apiUrl from './src/config';
+import apiUrl from './src/config.js';
 
 export default defineConfig({
   plugins: [react()],
